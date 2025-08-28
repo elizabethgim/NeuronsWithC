db00665aa12bbcf2d02f7d97f0134cd7e8f2cb8e
elizabethgim <yujingim@naver.com>
2025-08-28 22:09:03 +0900

    add: activation functions

--- source/NeuralNetwork.cpp
+++ source/NeuralNetwork.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 const int INPUT_NODE = 2;
 const int OUTPUT_NODE = 3;
@@ -10,13 +11,20 @@ double bias[OUTPUT_NODE] = {7, 8, 9};
 
 double output_b[OUTPUT_NODE];
 
+typedef enum _activation{
+    LINEAR = 0,
+    SIGMOID = 1,
+    RELU = 2
+} activation_t;
+
 void feed_forward(
     const double *input,
     const double *weight,
     const double *bias,
     double *output,
     const int OUTPUT_NODES,
-    const int INPUT_NODES
+    const int INPUT_NODES,
+    activation_t act
 ) {
 
     for(int j=0;j<OUTPUT_NODES;j++){
@@ -28,6 +36,15 @@ void feed_forward(
         
         prediction += bias[j];
         output[j] = prediction;
+
+         if(act==LINEAR){
+            output[j] = prediction;
+        }else if(act == SIGMOID){
+            output[j] = 1.0 /(1.0+exp(-prediction));
+        }else if(act==RELU){
+            output[j] = prediction > 0? prediction : 0;
+        }
+
         printf("output[%d] = %f\n", j, output[j]);
     }
 }
@@ -35,22 +52,32 @@ void feed_forward(
 void back_propagation(
     const double *output_b, // feed_forward output
     const double * weight,
+    double *input_f, // feed_forward input
     double *input_b, // backpropagation output
     const int OUTPUT_NODES,
-    const int INPUT_NODES
+    const int INPUT_NODES,
+    activation_t act
 ){
-    for(int i=0; i<INPUT_NODES;i++){
+    for(int j=0; j<INPUT_NODES;j++){
         double sum =0.0;
-        for(int j=0;j<OUTPUT_NODES;j++){
-            sum += output_b[j] * weight[i*OUTPUT_NODES+j];
+        for(int i=0;i<OUTPUT_NODES;i++){
+            sum += output_b[j] * weight[j*OUTPUT_NODES+i];
         }
-        input_b[i] = sum;
-        printf("output_b[%d] = %f\n", i, input_b[i]);
+
+        if(act==LINEAR){
+            input_b[j] = sum;
+        }else if(act == SIGMOID){
+            input_b[j] = input_f[j] * (1 - input_f[j]) *sum ;
+        }else if(act==RELU){
+            input_b[j] = (input_f[j] > 0? 1 : 0) * sum;
+        }
+
+        printf("input_b[%d] = %f\n", j, input_b[j]);
     }
 }
 
 int main(){
-    feed_forward(input, (const double*)weight, bias, output, OUTPUT_NODE, INPUT_NODE);
-    back_propagation(output, (const double*)weight, input, OUTPUT_NODE, INPUT_NODE);
+    feed_forward(input, (const double*)weight, bias, output, OUTPUT_NODE, INPUT_NODE, SIGMOID);
+    back_propagation(output, (const double*)weight, input, input, OUTPUT_NODE, INPUT_NODE, SIGMOID);
     return 0;
 }
