3cde163e387cd3b97b0ffe31d91a26fe91240159
elizabethgim <yujingim@naver.com>
2025-08-29 16:19:45 +0900

    fix: mute printfs except main

--- source/myannTest.cpp
+++ source/myannTest.cpp
@@ -23,32 +23,39 @@ double DoutputE[OUTPUT_NODES];
 
 double hidden_b[HIDDEN_NODES];
 
-double DweightE[INPUT_NODES][HIDDEN_NODES];
-double DbiasE[HIDDEN_NODES];
+double DweightOE[INPUT_NODES][HIDDEN_NODES];
+double DbiasOE[HIDDEN_NODES];
+double DweightHE[INPUT_NODES][HIDDEN_NODES];
+double DbiasHE[HIDDEN_NODES];
 
-double learning_rate = 0.05;
+double learning_rate = 0.5;
 
 int main(){
     for(long epoch=1; epoch<=1000000;epoch++){
         feed_forward(input, (const double*)weightH, biasH, hidden, INPUT_NODES, HIDDEN_NODES, SIGMOID);
         feed_forward(hidden, (const double*)weightO, biasO, output, HIDDEN_NODES, OUTPUT_NODES, SIGMOID);
         
-        double Error = get_error(target, output, OUTPUT_NODES);
+        double Error = get_error(target, output, OUTPUT_NODES, MSE);
 
         if(Error < 0.0001){
             printf("epoch = %d\n", epoch);
             printf("Error = %f\n", Error);
             printf("output[0] = %f\n", output[0]);
             printf("output[1] = %f\n", output[1]);
+            break;
         }
+        
 
         get_DoutputE(target, output, DoutputE, OUTPUT_NODES);
 
         prepare_back_propagation(DoutputE, output, output_b, OUTPUT_NODES, SIGMOID);
         back_propagation(output_b, (const double*)weightO, hidden, hidden_b, OUTPUT_NODES, INPUT_NODES, SIGMOID);
         
-        get_gradient((double *)DweightE, DbiasE, hidden_b, input, INPUT_NODES, HIDDEN_NODES);
-        apply_gradient((double *)DweightE, DbiasE, learning_rate, (double *)weightH, biasH, INPUT_NODES, OUTPUT_NODES);
+        get_gradients((double *)DweightOE, DbiasOE, output_b, hidden, HIDDEN_NODES, OUTPUT_NODES);
+        get_gradients((double *)DweightHE, DbiasHE, hidden_b, input, INPUT_NODES, HIDDEN_NODES);
+
+        apply_gradient((double *)DweightOE, DbiasOE, learning_rate, (double *)weightO, biasO, INPUT_NODES, OUTPUT_NODES);
+        apply_gradient((double *)DweightHE, DbiasHE, learning_rate, (double *)weightH, biasH, INPUT_NODES, OUTPUT_NODES);
     }
     
     return 0;
