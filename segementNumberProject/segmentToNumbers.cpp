78a7bf49763b12d9a1fe9b1d3a37ca2b8a4df13a
elizabethgim <yujingim@naver.com>
2025-08-29 16:34:40 +0900

    fix: unmatched input format

--- segementNumberProject/segmentToNumbers.cpp
+++ segementNumberProject/segmentToNumbers.cpp
@@ -4,8 +4,8 @@
 const int PATTERN_COUNT = 10;
 const int HIDDEN_NODES = 8;
 
+const int INPUT_NODES=7;
 const int OUTPUT_NODES=4;
-const int INPUT_NODES=9;
 
 double input[PATTERN_COUNT][INPUT_NODES] = {
 	{1, 1, 1, 1, 1, 1, 0}, // 0
@@ -38,14 +38,20 @@ double output[OUTPUT_NODES];
 double weight[INPUT_NODES][HIDDEN_NODES];
 double bias[HIDDEN_NODES];
 
-double output_backpropagation[OUTPUT_NODES];
-double input_backpropagation[INPUT_NODES];
+double output_b[OUTPUT_NODES];
+double input_b[INPUT_NODES];
+
+double DweightE[INPUT_NODES][HIDDEN_NODES];
+double DbiasE[INPUT_NODES][HIDDEN_NODES];
+
+double learning_rate = 0.5;
+double EPOCH_COUNT = 10000;
 
 int main(){
-	try {
-		forward(*input, *weight, bias, output, INPUT_NODES, OUTPUT_NODES, LINEAR);
-		return 0;
-	} catch(const char* error){
-		printf(error);
+
+	for(int h=0;h<HIDDEN_NODES;h++){
+		feed_forward(*input, *weight, bias, output, INPUT_NODES, OUTPUT_NODES, LINEAR);
+		back_propagation(output_b, (const double *)weight, *input, input_b, OUTPUT_NODES, INPUT_NODES, SIGMOID);
 	}
+
 }
